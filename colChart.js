be70451581de7e7d5061280a32f8cce7a77cde8f
Shen Lee <shenlee@westat.com>
2016-05-09 16:40:38 -0400

    fix

--- colChart.js
+++ colChart.js
@@ -31,7 +31,7 @@ function colChart() {
 			.attr("width", width)
 			.attr("height", height)
 			.append("g")
-				.attr("transform", "translate(0,0)");
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 		
 		// tooltips using d3-tip
 		
