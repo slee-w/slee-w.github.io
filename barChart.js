093efea550237f317dd73055b00e15cbbe868ce1
Shen Lee <shenlee@westat.com>
2016-05-09 17:07:08 -0400

    add bar chart function

--- barChart.js
+++ barChart.js
@@ -1,13 +1,12 @@
 // Reusable bar chart function
 
-function colChart() {
+function barChart() {
 	
 	// Options accessible to the caller
 	// These are the default values
 	
-	var	margin = {top: 20, right: 20, bottom: 60, left: 40},
-		width = 960 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom,
+	var	width = 960,
+		height = 500,
 		data = [];
 		
 	var updateWidth,
@@ -17,14 +16,29 @@ function colChart() {
 	function chart(selection) {
 		selection.each(function() {
 		
+		// margins; adjust width and height to account for margins
+		
+		var margin = {top: 20, right: 20, bottom: 60, left: 40},
+			widthAdj = width - margin.left - margin.right,
+			heightAdj = height - margin.top - margin.bottom;
+
+		// define bar spacing and bar height
+
+		var barPadding = 1;
+		
+		var barSpacing = heightAdj / data.length,
+			barHeight = barSpacing - barPadding,
+			maxValue = d3.max(data, function(d) { return d.pct; }),
+			widthScale = widthAdj / maxValue;
+		
 		// selections
 		
 		var dom = d3.select(this);
 		
 		var svg = dom.append("svg")
 			.attr("class", "bar-chart")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
+			.attr("width", width)
+			.attr("height", height)
 			.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 		
@@ -43,43 +57,40 @@ function colChart() {
 		
 		// axis scales and axes
 		
-		var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1),	
-			yScale = d3.scale.linear().range([height, 0]),
-			xAxis = d3.svg.axis().scale(xScale).orient("bottom"),
-			yAxis = d3.svg.axis().scale(yScale).orient("left").tickFormat(formatPercentNoDec);
+		var xScale = d3.scale.linear().range([0, widthAdj]),	
+			yScale = d3.scale.ordinal().range([heightAdj, 0]),
+			xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(formatPercentNoDec),
+			yAxis = d3.svg.axis().scale(yScale).orient("left");
 		
 		// domains
 		
-		xScale.domain(data.map(function(d, i) { return d.group; }));
-		yScale.domain([0, d3.max(data, function(d) { return d.pct; })]).nice();
+		xScale.domain([0, d3.max(data, function(d) { return d.pct; })]).nice();
+		yScale.domain(data.map(function(d, i) { return d.group; }));
 	
-		// draw columns
+		// draw bars
 		
-		var cols = svg.selectAll("rect.column")
+		var bars = svg.selectAll("rect.bar")
 			.data(data)
 			.enter()
 			.append("g")
 				.attr("transform", "translate(0,0)");
-		
-		var max = d3.max(data, function(d) { return d.pct; });
-		
-		cols.append("rect")
-			.attr("class","column")
-			.attr("x", function(d, i) { return xScale(d.group); })
-			.attr("width", xScale.rangeBand())
-			.attr("y", height)
-			.attr("height", 0)
+					
+		bars.append("rect")
+			.attr("class","bar")
+			.attr("x", margin.left)
+			.attr("width", 0)
+			.attr("y", function(d, i) { return i * barSpacing; })
+			.attr("height", barHeight)
 			.on("mouseover", tip.show)
 			.on("mouseout", tip.hide)
 			.transition()
 				.duration(500)
-				.attr("height", function(d) { return height - yScale(d.pct); })
-				.attr("y", function(d) { return yScale(d.pct); })
-				
+				.attr("width", function(d) { return d * widthScale; })
+								
 				// highlight if max
 			
-				.each("end", function(d) { if (d.pct == max) {
-					svg.select(".column")
+				.each("end", function(d) { if (d.pct == maxValue) {
+					svg.select(".bar")
 						.transition()
 							.duration(500)
 							.style("fill", "#8a89a6");
@@ -87,59 +98,62 @@ function colChart() {
 		
 		// draw axes
 	
+		xAxis.tickValues(xScale.domain());
+	
 		svg.append("g")
 			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis)
-			.selectAll(".tick text")
-				.call(wrap, xScale.rangeBand());
-	
-		yAxis.tickValues(yScale.domain());
+			.attr("transform", "translate(0," + heightAdj + ")")
+			.call(xAxis);
 	
 		svg.append("g")
 			.attr("class", "y axis")
 			.call(yAxis);
+			.selectAll(".tick text")
+				.call(wrap, yScale);
 		
 		// update functions
 		
 		updateWidth = function() {
 			
-			svg.attr("width", width);
-			cols.attr("x", function(d, i) { return xScale(d.group); })
-			cols.attr("width", xScale.rangeBand());
+			widthScale = width / maxValue;
+			
+			svg.attr("width", widthAdj);
+			bars.attr("width", function(d) { return d * widthScale; });
 			
 			};
 			
 		updateHeight = function() {
 			
-			svg.attr("height", height);
-			cols.attr("y", function(d) { return yScale(d.pct); })
-			cols.attr("height", function(d) { return height - yScale(d.pct); });
+			barSpacing = height / data.length;
+			barHeight = barSpacing - barPadding;
 			
+			svg.attr("height", heightAdj);
+			bars.attr("y", function(d, i) { return i * barSpacing; })
+						
 			};
 		
 		updateData = function() {
 		
-			xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-			yScale = d3.scale.linear().range([height, 0]);
-			xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+		var xScale = d3.scale.linear().range([0, widthAdj]),	
+			yScale = d3.scale.ordinal().range([heightAdj, 0]),
+			xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(formatPercentNoDec),
 			yAxis = d3.svg.axis().scale(yScale).orient("left");
 		
-			var update = svg.selectAll("rect.column")
+			var update = svg.selectAll("rect.bar")
 				.data(data);
 				
-			update.attr("x", function(d, i) { return xScale(d.group); })
-				.attr("width", xScale.rangeBand())
-				.attr("y", function(d) { return yScale(d.pct); })
-				.attr("height", function(d) { return height- yScale(d.pct); })
+			update.attr("x", margin.left)
+				.attr("width", function(d) { return d * widthScale; })
+				.attr("y", function(d, i) { return i * barSpacing; })
+				.attr("height", barHeight)
 		
 			update.enter()
 				.append("rect")
-				.attr("class","column")
-				.attr("x", function(d, i) { return xScale(d.group); })
-				.attr("width", xScale.rangeBand())
-				.attr("y", function(d) { return yScale(d.pct); })
-				.attr("height", function(d) { return height- yScale(d.pct); });
+				.attr("class","bar")
+				.attr("x", margin.left)
+				.attr("width", function(d) { return d * widthScale; })
+				.attr("y", function(d, i) { return i * barSpacing; })
+				.attr("height", barHeight);
 		
 			update.exit()
 				.remove();
