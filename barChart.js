2af661203d8d84795ca022b4886c03746f49ecf7
Shen Lee <shenlee@westat.com>
2016-05-10 15:47:04 -0400

    Responsive

--- barChart.js
+++ barChart.js
@@ -42,7 +42,7 @@ function barChart() {
 		var svg = dom.append("svg")
 			.attr("class", "bar-chart")
 			.attr("preserveAspectRatio", "xMinYMin meet")
-			.attr("viewBox", "0 0 " + (width) + " " + (height))
+			.attr("viewBox", "0 0 " + widthAdj + " " + heightAdj)
 			.attr("width", "100%")
 			.attr("height", "100%")
 			.append("g")
