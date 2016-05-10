b6c2de8661e5e55436f7c417d1e4fbd3258b9594
Shen Lee <shenlee@westat.com>
2016-05-10 15:45:15 -0400

    SVG

--- barChart.js
+++ barChart.js
@@ -43,8 +43,8 @@ function barChart() {
 			.attr("class", "bar-chart")
 			.attr("preserveAspectRatio", "xMinYMin meet")
 			.attr("viewBox", "0 0 " + (width) + " " + (height))
-			.attr("width", widthAdj)
-			.attr("height", heightAdj)
+			.attr("width", "100%" - marginLeft - margin.right)
+			.attr("height", "100%" - margin.top - margin.bottom)
 			.append("g")
 				.attr("transform", "translate(" + marginLeft + "," + margin.top + ")");
 		
