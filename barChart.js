f6358ec291120953bedbe64392385ad73e958b7f
skz87 <shenhan.lee@gmail.com>
2016-05-09 22:08:42 -0400

    % Fix

--- barChart.js
+++ barChart.js
@@ -62,7 +62,7 @@ function barChart() {
 		
 		var xScale = d3.scale.linear().range([0, widthAdj]),	
 			yScale = d3.scale.ordinal().range([heightAdj, 0]).rangeRoundBands([0, heightAdj], .1),
-			xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(formatPercentNoDec),
+			xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(formatPercent),
 			yAxis = d3.svg.axis().scale(yScale).orient("left");
 		
 		// domains
