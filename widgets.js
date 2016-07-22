7eb136c476bf0e53f705d6ff2d6b32a44cb8cc1d
slee-w <shenlee@westat.com>
2016-07-21 23:03:06 -0400

    Clear out, add migrant viz

--- widgets.js
+++
@@ -1,3 +0,0 @@
-window.onload = function() {
-		document.getElementById("race").innerHTML = '<div class="widget_graphics_container"><object data="R/race.html" style="width: 100%; height: 100%;"></object></div>';
-};
